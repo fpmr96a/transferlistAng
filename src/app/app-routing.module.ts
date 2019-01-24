f94e2d3c9c6f047aa2011e8a4190cff7fa149c05
fpmr96a <softwaresolutions@cox.net>
2019-01-24 13:15:19 -0500

    added navigation tabs and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyTransferlistComponent } from './components/my-transferlist/my-transferlist.component';
+import { FilteredTransferlistComponent } from './components/filtered-transferlist/filtered-transferlist.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { VacanciesComponent } from './components/vacancies/vacancies.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'mytransferlist', component: MyTransferlistComponent },
+  { path: 'filteredtransferlist', component: FilteredTransferlistComponent },
+  { path: 'vacancy', component: VacanciesComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: '', redirectTo: 'mytransferlist', pathMatch: 'full'},
+  { path: '**', redirectTo: 'mytransferlist', pathMatch: 'full'}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router} from '@angular/router'
+import { MatDialog } from '@angular/material/dialog';
+import { EmployeeProfileDialogComponent } from './components/admin/employee-profile-dialog/employee-profile-dialog.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'transferlistAng';
+export class AppComponent implements OnInit{
+  title = 'Transfer List';
+  navLinks: any[];
+  activeLinkIndex = -1;
+
+  constructor(private router: Router, 
+              public dialog: MatDialog) {
+    this.navLinks = [
+        {
+            label: 'My Transfer List',
+            link: './mytransferlist',
+            index: 0
+        }, {
+            label: 'Transfer List',
+            link: './filteredtransferlist',
+            index: 1
+        }, {
+          label: 'Vacancies',
+          link: './vacancy',
+          index: 1
+        }, {
+            label: 'Reports',
+            link: './reports',
+            index: 1
+         }, 
+    ];
+}
+ngOnInit(): void {
+  this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
+}
+
+openEmployeeProfile(): void {
+    let dialogRef = this.dialog.open(EmployeeProfileDialogComponent, {
+        width: '450px',
+        height: '470px',
+        disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed', result);
+    });
+  }   
+
 }
