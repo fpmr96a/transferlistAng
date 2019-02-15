da7454b2a6062b344c8ff456751c5ac1ed38aa3f
fpmr96a <softwaresolutions@cox.net>
2019-02-15 15:21:24 -0500

    added ngx-mask for phone number masking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import {NgxMaskModule} from 'ngx-mask'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,7 +45,8 @@ import { CreateTransferlistDialogComponent } from './components/my-transferlist/
     BrowserAnimationsModule,
     HttpClientModule,
     FlexLayoutModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot({})
     
   ],
   providers: [],
