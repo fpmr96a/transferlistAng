b11480bb3ae61ece1d75d5fcc4af97cd2d4be247
fpmr96a <softwaresolutions@cox.net>
2019-02-20 19:11:24 -0500

    filtered transfer list Job Class dropdown working

--- src/app/components/filtered-transferlist/filtered-transferlist.component.ts
+++ src/app/components/filtered-transferlist/filtered-transferlist.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+
+import { DataService } from '../../core/data.service';
+
+import { JobClass } from 'src/app/models/JobClass';
 
 @Component({
   selector: 'app-filtered-transferlist',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filtered-transferlist.component.scss']
 })
 export class FilteredTransferlistComponent implements OnInit {
+  jobClassFormControl = new FormControl('', Validators.required);
 
-  constructor() { }
+  jobClasses: JobClass[] = [];
+  selectedJobClass: JobClass;
+  selectedJobClassDescription: string;
+  selectedJobClassCode: string;
 
-  ngOnInit() {
+
+  errorMessage = '';
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+   this.dataService.getJobClasses().subscribe(
+      jobclasses => {
+        this.jobClasses = jobclasses;
+        //this.isLoading=false;
+        console.log("Filtered Job classes Returned" + JSON.stringify(this.jobClasses));
+      },
+      error => this.errorMessage = <any>error
+    ); 
+
+    // When an item in Job Class Dropdown selected, grab job class JSON string, load
+    // JSON object, and populate code and description properties
+    // ==============================================================================
+    /* this.jobClassFormControl.valueChanges.subscribe(
+      value => {
+        this.selectedJobClass = JSON.parse(value);
+        this.selectedJobClassDescription = this.selectedJobClass.description;
+        this.selectedJobClassCode = this.selectedJobClass.code;
+        
+        //this.isLoading=true;
+      }
+    ); */
   }
+
+  stringifyJobClassObject(selectedJobClass: any): string {
+    return JSON.stringify(selectedJobClass);
+   }
 
 }
