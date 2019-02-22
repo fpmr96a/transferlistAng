717e0905b7af1bdf1cbd6c58c5f927b5f1631022
fpmr96a <softwaresolutions@cox.net>
2019-02-22 15:28:57 -0500

    added filtered dropdowns but not done

--- src/app/components/filtered-transferlist/filtered-transferlist.component.ts
+++ src/app/components/filtered-transferlist/filtered-transferlist.component.ts
@@ -6,6 +6,9 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { DataService } from '../../core/data.service';
 
 import { JobClass } from 'src/app/models/JobClass';
+import { Facility } from 'src/app/models/Facility';
+import { FunctionalUnit } from 'src/app/models/FunctionalUnit';
+import { Shift } from 'src/app/models/shift';
 
 @Component({
   selector: 'app-filtered-transferlist',
@@ -14,11 +17,24 @@ import { JobClass } from 'src/app/models/JobClass';
 })
 export class FilteredTransferlistComponent implements OnInit {
   jobClassFormControl = new FormControl('', Validators.required);
+  facilityFormControl = new FormControl('', Validators.required);
+  functionalUnitFormControl = new FormControl('', Validators.required);
+  shiftFormControl = new FormControl('', Validators.required);
+  ftptFormControl = new FormControl('');
 
   jobClasses: JobClass[] = [];
   selectedJobClass: JobClass;
   selectedJobClassDescription: string;
   selectedJobClassCode: string;
+
+  selectedFacility: string = "-1";
+  selectedFunctionalUnit: string;
+  selectedShift: string;
+  selectedFTPT: string;
+
+  facilities: Facility[] = [];
+  functionalUnits: FunctionalUnit[] = [];
+  shifts: Shift[] = [];
 
   isLoading = true;
   errorMessage = '';
@@ -38,15 +54,55 @@ export class FilteredTransferlistComponent implements OnInit {
     // When an item in Job Class Dropdown selected, grab job class JSON string, load
     // JSON object, and populate code and description properties
     // ==============================================================================
-    /* this.jobClassFormControl.valueChanges.subscribe(
+    this.jobClassFormControl.valueChanges.subscribe(
       value => {
         this.selectedJobClass = JSON.parse(value);
         this.selectedJobClassDescription = this.selectedJobClass.description;
         this.selectedJobClassCode = this.selectedJobClass.code;
+
+        this.getFunctionalUnits('-1', this.selectedJobClassCode);
+       // *** TODO: problem - Need to reset the SHIFT when the job class changes. Figure this out.
+        this.dataService.getTransferFacilityByJobcode(this.selectedJobClassCode).subscribe(
+          returnedFacilities => {
+            this.facilities = returnedFacilities;
+            console.log("facilities Returned" + JSON.stringify(this.facilities));
+          },
+          error => this.errorMessage = <any>error
+        ); 
+    
+        this.dataService.getShiftByJobcode(this.selectedJobClassCode).subscribe(
+          returnedShifts => {
+            this.shifts = returnedShifts;
+            console.log("shifts Returned" + JSON.stringify(this.shifts));
+          },
+          error => this.errorMessage = <any>error
+        ); 
+    
+       
         
+        // When an item in the Facility Dropdown selected, grab facility code and call
+        // web api to get Functional Units.
+        // ==============================================================================
+        this.facilityFormControl.valueChanges.subscribe(
+          value => {
+            this.selectedFacility = value;
+            this.getFunctionalUnits(this.selectedFacility, this.selectedJobClassCode);
+            
+          }
+        );
         //this.isLoading=true;
       }
-    ); */
+    );
+  }
+
+  getFunctionalUnits(facility: string, jobcode: string) {
+    this.dataService.getFunctionalUnitByFacilityByJobcode(this.selectedFacility,this.selectedJobClassCode).subscribe(
+      returnedFuntionalUnits => {
+        this.functionalUnits = returnedFuntionalUnits;
+        console.log("Functional Units Returned" + JSON.stringify(this.functionalUnits));
+      },
+      error => this.errorMessage = <any>error
+    ); 
   }
 
   stringifyJobClassObject(selectedJobClass: any): string {
