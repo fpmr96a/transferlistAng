11cdbb64920946ff8f8937f9fa9cc5c8d387f2c4
fpmr96a <softwaresolutions@cox.net>
2019-02-28 22:52:25 -0500

    populating job class dropdown

--- src/app/components/vacancies/vacancies.component.ts
+++ src/app/components/vacancies/vacancies.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms'
-import { MatDialog } from '@angular/material/dialog';
 import { DataService } from 'src/app/core/data.service';
+import { JobClass } from 'src/app/models/JobClass';
+import { Facility } from 'src/app/models/Facility';
+import { FunctionalUnit } from 'src/app/models/FunctionalUnit';
+import { Shift } from 'src/app/models/shift';
 
 @Component({
   selector: 'app-vacancies',
@@ -9,14 +12,44 @@ import { DataService } from 'src/app/core/data.service';
   styleUrls: ['./vacancies.component.scss']
 })
 export class VacanciesComponent implements OnInit {
-  bordersControl = new FormControl();
-  constructor(public dialog: MatDialog,
-             public dataService: DataService) { }
+  jobClassFormControl = new FormControl('', Validators.required);
+  facilityFormControl = new FormControl('', Validators.required);
+  functionalUnitFormControl = new FormControl('', Validators.required);
+  shiftFormControl = new FormControl('', Validators.required);
+  ftptFormControl = new FormControl('');
+
+  jobClasses: JobClass[] = [];
+  selectedJobClass: JobClass;
+  selectedJobClassDescription: string;
+  selectedJobClassCode: string;
+
+  selectedFacility: string = "-1";
+  selectedFunctionalUnit: string;
+  selectedShift: string;
+  selectedFTPT: string;
+
+  facilities: Facility[] = [];
+  functionalUnits: FunctionalUnit[] = [];
+  shifts: Shift[] = [];
+
+  isLoading = true;
+  errorMessage = '';
+  constructor(public dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getJobClasses().subscribe(
+      jobclasses => {
+        this.jobClasses = jobclasses;
+        this.isLoading=false;
+        console.log("Filtered Job classes Returned" + JSON.stringify(this.jobClasses));
+      },
+      error => this.errorMessage = <any>error
+    ); 
   }
 
-  
+  stringifyJobClassObject(selectedJobClass: any): string {
+    return JSON.stringify(selectedJobClass);
+   }
   
 
 }
