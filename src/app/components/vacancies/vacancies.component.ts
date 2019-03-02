9d7905e9308a31d07676ebf526215e487c0e0509
fpmr96a <softwaresolutions@cox.net>
2019-03-02 08:23:36 -0500

    selected filtered job class desc shown as button text

--- src/app/components/vacancies/vacancies.component.ts
+++ src/app/components/vacancies/vacancies.component.ts
@@ -20,7 +20,7 @@ export class VacanciesComponent implements OnInit {
 
   jobClasses: JobClass[] = [];
   selectedJobClass: JobClass;
-  selectedJobClassDescription: string;
+  selectedJobClassDescription: string = "Select Job Class"
   selectedJobClassCode: string;
 
   selectedFacility: string = "-1";
@@ -45,6 +45,27 @@ export class VacanciesComponent implements OnInit {
       },
       error => this.errorMessage = <any>error
     ); 
+  
+    // When an item in Job Class Dropdown selected, grab job class JSON string, load
+    // JSON object, and populate code and description properties
+    // ==============================================================================
+    this.jobClassFormControl.valueChanges.subscribe(
+      value => {
+        this.selectedJobClass = JSON.parse(value);
+        this.selectedJobClassDescription = this.selectedJobClass.description;
+        this.selectedJobClassCode = this.selectedJobClass.code;
+        
+        //this.isLoading=true;
+      }
+    );
+
+    this.ftptFormControl.valueChanges.subscribe(
+      value => {
+        this.selectedFTPT = JSON.parse(value);
+        console.log("Selected FT PT: " + this.selectedFTPT);
+       // console.log("Selected property:" + this.ftptFormControl.)
+      }
+    )
   }
 
   stringifyJobClassObject(selectedJobClass: any): string {
