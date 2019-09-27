dc0b35628f5f0d0a4b18c86f2ff7f47e24f6f50c
fpmr96a <softwaresolutions@cox.net>
2019-09-27 11:27:46 -0400

    populating job class dropdown on vacancy

--- src/app/components/vacancies/vacancies.component.ts
+++ src/app/components/vacancies/vacancies.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms'
 
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 import { DataService } from '../../core/data.service';
 
 import { Vacancy } from '../../models/Vacancy';
+import { JobClass } from 'src/app/models/JobClass';
 
 @Component({
   selector: 'app-vacancies',
@@ -12,12 +14,65 @@ import { Vacancy } from '../../models/Vacancy';
   styleUrls: ['./vacancies.component.scss']
 })
 export class VacanciesComponent implements OnInit {
+  jobClassFormControl = new FormControl('', Validators.required);
+  selectedRowIndex: number;
+  errorMessage = '';
+  isLoading = true;
+
+  jobClasses: JobClass[] = [];
+  selectedJobClass: JobClass;
+  selectedJobClassDescription: string;
+  selectedJobClassCode: string;
 
   vacancies: Vacancy[] = [];
+  displayedColumns: string[] = ['pcn', 'facilityShortDescription', 'functionalUnitDesc', 'approvalDateVMS', 'shiftDesc', 'hours', 'ftPT','createDt'];
 
-  constructor() { }
+  dataSource = new MatTableDataSource();
 
-  ngOnInit() {
-  }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getJobClasses().subscribe(
+       jobclasses => {
+         this.jobClasses = jobclasses;
+         this.isLoading=false;
+         console.log("Job classes Returned" + JSON.stringify(this.jobClasses));
+       },
+       error => this.errorMessage = <any>error
+     ); 
+ 
+     // When an item in Job Class Dropdown selected, grab job class JSON string, load
+     // JSON object, and populate code and description properties
+     // ==============================================================================
+     this.jobClassFormControl.valueChanges.subscribe(
+       value => {
+         console.log('selected job code is: ' + value);
+         if (value.trim() == "")
+         {
+           this.selectedJobClassDescription = "";
+           this.selectedJobClassCode = "";
+           //this.GetMyTransferListByJobCode("0000");  /* Clear grid if Job Code not selected */
+         }
+         else
+         {
+         this.selectedJobClass = JSON.parse(value);
+         this.selectedJobClassDescription = this.selectedJobClass.description;
+         this.selectedJobClassCode = this.selectedJobClass.code;
+         //this.GetMyTransferListByJobCode(this.selectedJobClassCode);
+         this.isLoading=true;
+         }
+ 
+         
+         
+       }
+     );
+   }
+
+   stringifyJobClassObject(selectedJobClass: any): string {
+    return JSON.stringify(selectedJobClass);
+   }
 
 }
